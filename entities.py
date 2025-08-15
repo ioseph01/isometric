2286f64f36e45a0794871e3702bbda1b97d40d96
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2025-08-15 14:16:03 -0700

    In-between tile movement

--- entities.py
+++ entities.py
@@ -1,119 +1,178 @@
-import random
-from utils import *
+from utils import sign, to_iso
+import pygame
+import random 
 
+def move(pos, dx, dy):
+    new_x, new_y = pos[0] + dx, pos[1] + dy
+    if abs(new_y) + abs(new_x) / 2 > 4:
+        return [pos[0] - 8 * sign(new_x), pos[1] - 4 * sign(new_y)]
+    return [new_x, new_y]
 
-class Tile:
-    def __init__(self, maze, z, x, y):
-        self.maze = maze
-        self.pos = z
-        self.x, self.y = x, y
-        self.sprite_variant = self.maze.sprite_variant if self.maze.sprite_variant is not None else stable_randint(x, y, z, self.maze.game.level)
-       
+def in_tile(pos, dx, dy):
+    return abs(pos[0] + dx) / 2 + abs(pos[1] + dy) <= 4
+
+def coord_transform(x,y):
+    result = [-2*x + 2*y, x + y]
+    return [-2*x + 2*y, x + y]
+
+class Entity:
+    def __init__(self, game, pos, sprite, hp=1, render_offset=[0,0], e_type=None):
+        self.game = game
+        self.pos = list(pos)
+        self.sprite = sprite
+        self.path = []
+        self.hp = 1
+        self.render_offset = render_offset
+        self.movement_offset = [0,0]
+        self.type = e_type
+        self.flip = True
+        self.tick = [0,300]    
+
     @property
-    def z(self):
-        return self.pos
+    def at(self):
+        return self.game.maze.maze[self.y][self.x]
+
+    @property
+    def x(self):
+        return self.pos[0]
     
-    def __lt__(self, other):
-        return self.z < other.z
-    
     @property
-    def type(self):
-        return "Tile"
-
-    @property
-    def z1(self):
-        return self.pos
-    
-    @property
-    def all_z(self):
-        return [self.pos]
-
-    @property
-    def adjacent_cells(self):
-        cells = set()
-        dirs = [(-1,0,range(2)),(1,0,range(-1,1)),(0,1,range(-1,1)),(0,-1,range(2)),(2,1,range(1,3)),(1,2,range(1,3)),(-2,-1,range(-2,0)),(-1,-2,range(-2,0))]
-        for dx, dy, dz in dirs:
-            if not self.maze.in_range([dx + self.x, dy + self.y]):
-                continue
-            cell = self.maze.maze[dy + self.y][dx + self.x]
-            if cell is not None:
-                if abs(dx) <= 1 and abs(dy) <= 1:
-                    for diff in self.z_check(cell):
-                        if diff in dz:
-                            cells.add(cell)
-                elif abs(dx) >= 1 and abs(dy) >= 1:
-                    for diff in self.z_check(cell):
-                        if diff in dz:
-                            cells.add(cell)
-                            
-
-        return cells
+    def y(self):
+        return self.pos[1]
     
     @property
     def render_pos(self):
         ''' Returns calculated x and y of to_iso adjusted to tile height '''
-        iso_x, iso_y = to_iso(self.x, self.y, self.maze.TILE_WIDTH, self.maze.TILE_HEIGHT)
-        iso_y -= self.z * (self.maze.TILE_HEIGHT // 2)  
+        iso_x, iso_y = to_iso(self.x, self.y, self.game.maze.TILE_WIDTH, self.game.maze.TILE_HEIGHT)
+        iso_y -= self.at.z * (self.game.maze.TILE_HEIGHT // 2)  
         return [iso_x, iso_y]
-
-
-    def render(self, screen, h, offset, wall_spacing):
+    
+    def render(self, screen, offset):
         x,y = self.render_pos
-        for i in reversed(range(h)):
-            screen.blit(self.maze.assets[self.type][self.sprite_variant], (x + offset[0], y + offset[1] + wall_spacing * i))
-            
+        screen.blit(pygame.transform.flip(self.sprite, self.flip, False), (x + offset[0] + self.render_offset[0] + self.movement_offset[0], y + offset[1] + self.render_offset[1] + self.movement_offset[1]))
+
     def update(self):
         pass
-    
-    def z_check(self, other):
-        if self.type == 'Tile' and other.type == 'Tile':
-            return [other.z - self.z]
-        elif other.type == 'Elevator':
-            return [other.z2 - self.z, other.z1 - self.z]
-        elif self.type == 'Elevator':
-            return [other.z - self.z1, other.z - self.z2]
-
-
-class Elevator(Tile):
-    def __init__(self, game, z, other_z, x, y):
-        super().__init__(game, int(z), x, y)
-        self.z2 = int(other_z)
-        self.current_z = z
-        self.direction = -1
-        self.time_stopped = 0
         
-    def __bool__(self): # returns if stopped
-        return self.time_stopped <= 0
-        
-    @property
-    def type(self):
-        return "Elevator"
-    
-    @property
-    def z(self):
-        return self.current_z
-        
-    @property
-    def z1(self):
-        return self.pos
-    
-    @property
-    def all_z(self):
-        return [self.z1, self.z2]
 
+class Gem(Entity):
     def update(self):
-        if self.time_stopped <= 0:
-            self.current_z += self.direction * 0.1
-            self.current_z = round(self.current_z,2)
-        
-        
-            if (self.direction > 0 and self.current_z >= self.pos) or (self.direction < 0 and self.current_z <= self.z2):
-                self.direction *= -1
-                self.time_stopped = 60
-        else:
-            self.time_stopped -= 1 
+        if self.pos == self.game.player.pos:
+            self.hp = 0
 
-    def render(self, screen, h, offset, wall_spacing):
-        super().render(screen, 1, offset, wall_spacing)
+
+class Player(Entity):
+    @property
+    def next_in_path(self):
+        if self.path == [] or self.path is None:
+            return None
+        
+        return self.path[0]
+    
+
+    def update(self, tick, movement=[0,0,0,0]):
+        
+        if tick % 2 == 0:
+            diff = [movement[1] - movement[3], movement[0] - movement[2]]
+            
+            if diff[0] != 0 and diff[1] != 0:
+                old = random.choice([ [ 0, diff[1] ], [ diff[0], 0 ] ])
+            else:
+                old = diff
+            if old != [0,0]:
+                diff = coord_transform(*old)
+                
+                if sign(diff[0]) == -1:
+                    self.flip = False
+                elif sign(diff[0]) == 1:
+                    self.flip = True
+                
+                if not in_tile(self.movement_offset, *diff):
+                    new_player, diff2 = self.game.maze.border_check(self.pos, old)
+                    if self.pos != new_player:
+                        old = self.movement_offset
+                        self.movement_offset = move(self.movement_offset, *diff)
+                        self.game.render_offset[0] -= diff[0]
+                        self.game.render_offset[1] -= diff[1] + diff2[1] - 5 * sign(diff2[1])
+                    
+                    self.pos = new_player
+
+                else:
+                    old = self.movement_offset
+                    self.movement_offset = move(self.movement_offset, *diff)
+                    self.game.render_offset[0] -= self.movement_offset[0] - old[0]
+                    self.game.render_offset[1] -= self.movement_offset[1] - old[1]
             
 
+
+class Enemy(Entity):
+    
+    def greedy_move_toward_player(self):
+        def cheb_dist(pos):
+            return max(abs(self.game.player.x - pos[0]), abs(self.game.player.y - pos[1]))
+    
+        current_dist = cheb_dist(self.pos)
+        neighbors = list(self.at.adjacent_cells)
+        candidates = []
+        for n in neighbors:
+            dist = cheb_dist([n.x, n.y])
+            improvement = current_dist - dist
+            if improvement >= 0:
+                weight = improvement + 1
+                candidates.append(([n.x, n.y, min(n.all_z, key=lambda x: abs(x - self.at.z))], weight))
+
+        if not candidates:
+            return [self.x, self.y, self.at.z]
+
+        positions, weights = zip(*candidates)
+        chosen = random.choices(positions, weights=weights, k=1)[0]
+        return chosen
+
+        
+    def goToNext(self, next_coord):
+        cell, next_cell = self.at, self.game.maze.maze[next_coord[1]][next_coord[0]]
+        if cell == next_cell and self.movement_offset != [0,0]:
+            self.movement_offset[0] -= 2 * sign(self.movement_offset[0])
+            self.movement_offset[1] -= 1 * sign(self.movement_offset[1])
+        elif next_cell.z == next_coord[2]:
+            old = [next_coord[0] - self.x, next_coord[1] - self.y]
+            if old == [0,0]:
+                new_pos, diff = self.game.maze.border_check(self.pos, [next_coord[0] - self.x, next_coord[1] - self.y])
+                if list(next_coord[:2]) == new_pos and self.movement_offset == [0,0]:
+                    self.path.pop(0)
+            
+                self.pos = new_pos
+            else:
+                diff = coord_transform(*old)
+                
+                if sign(diff[0]) == -1:
+                    self.flip = False
+                elif sign(diff[0]) == 1:
+                    self.flip = True
+                
+                if not in_tile(self.movement_offset, *diff):
+                    new_player, diff2 = self.game.maze.border_check(self.pos, old)
+                    if self.pos != new_player:
+                        old = self.movement_offset
+                        self.movement_offset = move(self.movement_offset, *diff)
+                    
+                    self.pos = new_player
+
+                else:
+                    old = self.movement_offset
+                    self.movement_offset = move(self.movement_offset, *diff)
+                
+                
+    def update(self, entity_dict):
+        if self.path is None or self.path == []:
+            self.path = [self.greedy_move_toward_player()]
+        elif self.game.tick[0] % 5 == 0:
+            next_coord = self.path[0]
+            if (next_coord[0], next_coord[1]) in entity_dict:
+                for e in entity_dict[(next_coord[0], next_coord[1])]:
+                    if e.type == 'Enemy':
+                        break
+                else:    
+                    self.goToNext(next_coord)
+            else:
+                self.goToNext(next_coord)
