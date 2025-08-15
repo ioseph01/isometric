72734b1ebffe85d32cc657ba67e54562c9a806b6
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2025-08-15 14:10:53 -0700

    Sprite Color Table for Replacement & Biased/random hash for Tile Sprites

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import pygame
+from hashlib import md5
+
 
 def sign(x):
     if x > 0:
@@ -14,10 +16,39 @@ def coord_add(a, b):
     return (a[0] + b[0], a[1] + b[1])
 
 
+BASE_IMG_PATH = 'img/'
+
 def load_image(path):
-    img = pygame.image.load('img/' + path).convert()
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0,0,0))
     return img
+
+
+def load_images(path):
+    images = []
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
+        images.append(load_image(path + '/' + img_name))
+        
+    return images
+
+
+def replace_colors(surface, color_map):
+    """Replaces all pixels matching old_color with new_color given color map; preserves alpha."""
+    
+    if color_map is None:
+        return surface
+    new_surface = surface.copy()
+    width, height = new_surface.get_size()
+
+    for x in range(width):
+        for y in range(height):
+            current_color = new_surface.get_at((x, y))
+            new_rgb = color_map.get(current_color[:3])
+            if new_rgb:
+                new_surface.set_at((x, y), new_rgb + (current_color[3],))  
+
+    return new_surface
+
 
 def coord_add(a, b):
     return (a[0] + b[0], a[1] + b[1])
@@ -45,3 +76,11 @@ def cell_type(pos, maze, cell_type='Tile'):
     if cell_valid(pos, maze):
         return cell_type == maze.maze[pos[1]][pos[0]].type
     return False    
+
+
+def stable_randint(x, y, z, level, min_val=0, max_val=3):
+
+    s = f"{x},{y},{z},{level}"
+    h = md5(s.encode()).digest()
+    val = int.from_bytes(h[:4], 'little')
+    return val % (max_val - min_val + 1) + min_val
