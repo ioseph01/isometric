6eb1321c7243da24d942051c6e9098e1a37f5ee1
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2025-08-19 12:08:38 -0700

    Friendly Tile

--- structures.py
+++ structures.py
@@ -2,12 +2,23 @@ import random
 from utils import *
 
 
+def reduce_coord(x, y):
+    if abs(x) <= 1 and abs(y) <= 1:
+        return x, y
+    if abs(x) > 1:
+        return int(x/abs(x)), 0  
+    else:
+        return 0, int(y/abs(y))   
+        
+    
 class Tile:
     def __init__(self, maze, z, x, y):
         self.maze = maze
         self.pos = z
         self.x, self.y = x, y
-        self.sprite_variant = self.maze.sprite_variant if self.maze.sprite_variant is not None else stable_randint(x, y, z, self.maze.game.level)
+        sprite_variant = self.maze.sprite_variant if self.maze.sprite_variant is not None else stable_randint(x, y, z, self.maze.game.level)
+        sprite = self.type
+        self.sprite = self.maze.assets[sprite][sprite_variant]
        
     @property
     def z(self):
@@ -30,6 +41,7 @@ class Tile:
 
     @property
     def adjacent_cells(self):
+        ''' Returns cells that are connected to the tile '''
         cells = set()
         dirs = [(-1,0,range(2)),(1,0,range(-1,1)),(0,1,range(-1,1)),(0,-1,range(2)),(2,1,range(1,3)),(1,2,range(1,3)),(-2,-1,range(-2,0)),(-1,-2,range(-2,0))]
         for dx, dy, dz in dirs:
@@ -45,10 +57,27 @@ class Tile:
                     for diff in self.z_check(cell):
                         if diff in dz:
                             cells.add(cell)
-                            
-
         return cells
     
+
+    def get_neighbor(self, dx, dy):
+        ''' Gets the neighboring cell in the direction of dx and dy to the tile.
+            Assumes dx,dy is reduced
+        '''
+        def reduce_coord(x, y):
+            if abs(x) <= 1 and abs(y) <= 1:
+                return x, y
+            if abs(x) > 1:
+                return int(x/abs(x)), 0  
+            else:
+                return 0, int(y/abs(y))   
+            
+        neighbors = self.adjacent_cells
+        for n in neighbors:
+            x,y = reduce_coord(n.x - self.x, n.y - self.y)
+            if x == dx and y == dy:
+                return n
+        return None
     @property
     def render_pos(self):
         ''' Returns calculated x and y of to_iso adjusted to tile height '''
@@ -59,19 +88,49 @@ class Tile:
 
     def render(self, screen, h, offset, wall_spacing):
         x,y = self.render_pos
-        for i in reversed(range(h)):
-            screen.blit(self.maze.assets[self.type][self.sprite_variant], (x + offset[0], y + offset[1] + wall_spacing * i))
+        for i in reversed(range(0,h)):
+            screen.blit(self.sprite, (x + offset[0], y + offset[1] + wall_spacing * i))
             
     def update(self):
         pass
     
     def z_check(self, other):
-        if self.type == 'Tile' and other.type == 'Tile':
+        if self.type != 'Elevator' and other.type != 'Elevator':
             return [other.z - self.z]
         elif other.type == 'Elevator':
             return [other.z2 - self.z, other.z1 - self.z]
         elif self.type == 'Elevator':
             return [other.z - self.z1, other.z - self.z2]
+        
+
+    def get_outline(self):
+        ''' Returns the points that make up its top grid lines '''
+        def generate_points(x_start=0, y_start=0, slope=1, n=10):
+            return [(x_start + i, y_start + slope * ((i + 1) // 2)) for i in range(n)]
+        
+        _ = {
+            ( 0,-1): generate_points(x_start=0,  y_start=5,  slope=-1), 
+            ( 0, 1): generate_points(x_start=10, y_start=10, slope=-1),
+            (-1, 0): generate_points(x_start=10, y_start=0,  slope= 1), 
+            ( 1, 0): generate_points(x_start=0,  y_start=5,  slope= 1), 
+            }
+        border_points = set()
+        if self.type != 'Elevator':
+            neighbors = self.adjacent_cells
+            borders = {k for k in _}
+                    
+            for neighbor in neighbors:
+                if neighbor.type != 'Elevator' and abs(neighbor.z1 - self.z1) != 1:
+                    dx,dy = reduce_coord(neighbor.x - self.x, neighbor.y - self.y)
+                    borders.discard((dx,dy))
+                            
+            new_surface = self.sprite.copy()
+            for dx,dy in borders:
+                points = _[(dx,dy)]
+                pos = self.render_pos
+                border_points |= set([(x + pos[0], y + pos[1]) for x,y in points])
+
+        return border_points
 
 
 class Elevator(Tile):
@@ -115,5 +174,53 @@ class Elevator(Tile):
 
     def render(self, screen, h, offset, wall_spacing):
         super().render(screen, 1, offset, wall_spacing)
-            
+        
+       
 
+class Player_Tile(Tile):
+    def __init__(self, maze, z, x, y):
+        self.maze = maze
+        self.pos = z
+        self.x, self.y = x, y
+        self.sprite = self.maze.assets[self.type]
+        intensity = .6
+        alpha = int(255 * intensity)
+        self.sprite.set_alpha(alpha)
+
+    @property
+    def type(self):
+        return 'Player_Tile'
+
+    def render(self, screen, h, offset, wall_spacing):
+        x,y = self.render_pos
+        screen.blit(self.sprite, (x + offset[0], y + offset[1]), special_flags=pygame.BLEND_ALPHA_SDL2)
+        
+
+    def get_outline(self):
+        ''' Returns the points that make up its top grid lines '''
+        def generate_points(x_start=0, y_start=0, slope=1, n=10):
+            return [(x_start + i, y_start + slope * ((i + 1) // 2)) for i in range(n)]
+        
+        _ = {
+            ( 0,-1): generate_points(x_start=0,  y_start=5,  slope=-1), 
+            ( 0, 1): generate_points(x_start=10, y_start=10, slope=-1),
+            (-1, 0): generate_points(x_start=10, y_start=0,  slope= 1), 
+            ( 1, 0): generate_points(x_start=0,  y_start=5,  slope= 1), 
+            }
+        border_points = set()
+        neighbors = self.adjacent_cells
+        borders = {k for k in _}
+                    
+        new_surface = self.sprite.copy()
+        for dx,dy in borders:
+            points = _[(dx,dy)]
+            pos = self.render_pos
+            border_points |= set([(x + pos[0], y + pos[1]) for x,y in points])
+
+        return border_points
+
+class Enemy_Tile(Tile):
+    @property
+    def type(self):
+        return 'Enemy_Tile'
+        
